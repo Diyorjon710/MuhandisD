30d7790edb754df4ceb2d7ec0c8a994d583e7424
Diyorjon <baxtiyorovich710@gmail.com>
2024-04-16 13:16:32 +0500

    finish

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'http://127.0.0.1:8000/', 'http://localhost:8000/', 'localhost']
 
 # Application definition
 
